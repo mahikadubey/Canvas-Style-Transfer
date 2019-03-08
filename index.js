8b305aac7a6ac977a3415a9e2f8f1ca494d58f2e
mahika <mahikadubey@users.noreply.github.com>
2019-03-08 11:37:42 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var canvas = document.querySelector("canvas"),
+    context = canvas.getContext("2d"),
+    width = canvas.width,
+    height = canvas.height;
+
+var brush = d3.brush();
+
+var svg = d3.select("svg");
+
+var image = new Image;
+image.src = "cat-cute.jpg";
+image.onload = loaded;
+
+function loaded() {
+
+  context.drawImage(this, 0, 0, this.width, this.height, 0, 0, canvas.width, canvas.height);
+
+  console.log('drawing image');
+
+  svg.append("g")
+      .attr("class", "brush")
+      .call(brush)
+      .call(brush.move, [[307, 167], [611, 539]]);
+}
