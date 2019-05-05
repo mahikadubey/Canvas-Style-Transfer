e9d9c5ba0ab07a972e139fc868982d5434f81ece
Mahika Dubey <mahika@Mahikas-MacBook-Air.local>
2019-05-04 23:23:53 -0700

    new model cstamps

--- Compositing-Stamps/index.js
+++ Compositing-Stamps/index.js
@@ -26,7 +26,8 @@ let STYLES = ['udnie', 'scream', 'wave',
               'wreck', 'matta', 'mathura',
               'la_muse', 'bruises', 'hennessy',
               'hennessy_zoom', 'minard_immigration',
-              'tufte_flatland', 'scrapbook1_glupi', 'clear'] // 'matilde_perez', 'rain_princess'
+              'tufte_flatland', 'scrapbook1_glupi',
+              'scrapbook2_glupi', 'clear'] // 'matilde_perez', 'rain_princess'
 
 
 d3.select('#brushes')
